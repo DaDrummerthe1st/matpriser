215efcb1ab93c684c8df4bfbb5cf9bd3fe2fbb89
Joakim Reuterborg <joakim.reuterborg@gmail.com>
2025-02-20 23:10:22 +0100

    trying db-connection

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 import streamlit as st
+import pandas as pd
 
 from libraries.database_management import connect_mongo
 
 st.header('Registrera ny vara')
 
-# collection = connect_mongo.connect_to_a_mongo()
+collection = connect_mongo.connect_to_a_mongo()
 
-# mydic = {
-#     'deal': {
-#         'product': 'Bregott 600 g',
-#         'pris': '39kr',
-#         'butik': 'Ica Oj Söder'
-#     }}
+mydic = {
+    'deal': {
+        'product': 'Bregott 600 g',
+        'pris': '39kr',
+        'butik': 'Ica Oj Söder'
+    }}
 
-# collection.insert_one(mydic)
+collection.insert_one(mydic)
 
-# findings = collection.find()
-# st.write(findings)
+findings = collection.find()
+st.write(findings)
 
 register = st.form('key')
 col1, col2, col3 = st.columns([3,1,3], vertical_alignment='bottom')
